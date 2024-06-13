89990b718bf23a4bdb0fbc796f3dc089204a7d68
Vihang Karekar <vihang.karekar@gmail.com>
2024-06-13 09:03:03 -0400

    message

--- SampleFolder/followersCount/followers_count.js
+++ SampleFolder/followersCount/followers_count.js
@@ -6,12 +6,20 @@ function increaseCount() {
     checkCountValue(); // Check count value and display messages
 }
 
+function resetCount() {
+    count = 0; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
 
 function checkCountValue() {
-    if (count === 10) {
+    if (count === 0){
+        alert("Your Instagram post followers were reset.");
+    } else if (count === 10) {
       alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
       alert("Your Instagram post gained 20 followers! Keep it up!");
