97010bf4a859e0d9551d2a4927505b025fbd08a6
Vihang Karekar <vihang.karekar@gmail.com>
2024-06-14 03:51:20 -0400

    message

---
+++ SampleFolder/restaurantMenu/restaurant_menu.js
@@ -0,0 +1,22 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+document.getElementById('breakfastTotalItems').innerHTML = "Total items: "+breakfastMenu.length;
+
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+document.getElementById('maincourseTotalItems').innerHTML = "Total items: "+mainCourseMenu.length;
+
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+document.getElementById('dessertTotalItems').innerHTML = "Total items: "+dessertMenu.length;
+
